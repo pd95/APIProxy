44f265027124c8f51ce7ff434174c752797b2608
Philipp <pd95@users.noreply.github.com>
2025-12-06 11:57:36 +0100

    Make hostname configurable to allow attachment to specific IP

--- Sources/APIProxy/configure.swift
+++ Sources/APIProxy/configure.swift
@@ -3,6 +3,7 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // Allow port configuration via environment
+    app.http.server.configuration.hostname = Environment.get("HOST") ?? "127.0.0.1"
     app.http.server.configuration.port = Environment.get("PORT").flatMap(Int.init) ?? 8080
 
     // Register ProxyService lifecycle handler
