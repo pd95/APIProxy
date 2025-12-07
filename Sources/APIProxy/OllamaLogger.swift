c58a317ca2026049385324925188033cd57b2445
Philipp <pd95@users.noreply.github.com>
2025-12-07 12:27:57 +0100

    Try to implement OpenAI chunked messages

--- Sources/APIProxy/OllamaLogger.swift
+++ Sources/APIProxy/OllamaLogger.swift
@@ -9,9 +9,27 @@ import NIOCore
 import Logging
 import Foundation
 
+private extension ByteBuffer {
+    mutating func readDataUntilNewline() -> Data? {
+        guard let index = self.readableBytesView.firstIndex(of: 10) else {
+            return nil
+        }
+
+        let length = index - self.readerIndex
+        let data = self.readData(length: length)
+        _ = self.readBytes(length: 1) // das "\n" konsumieren
+
+        return data
+    }
+}
+
 struct OllamaLogger {
 
-    static let logger = Logger(label: "OllamaAPI")
+    static let logger = {
+        var logger = Logger(label: "OllamaAPI")
+        logger.logLevel = .info
+        return logger
+    }()
 
     let method: String
     let url: String
@@ -29,12 +47,48 @@ struct OllamaLogger {
     }
 
     func log(buffer: ByteBuffer) {
-        let bodyData = Data(buffer: buffer)
+        let isOpenAICompatibility = url.prefix(3) == "/v1"
+        var readBuffer = buffer
 
-        if let jsonObject: [String : Any] = try? JSONSerialization.jsonObject(with: bodyData, options: []) as? [String: Any] {
-            //print(jsonObject)
-            if let message = jsonObject[
-                "message"] as? [String: Any], let done = jsonObject["done"] as? Bool, done == false {
+        // Some requests start with a plain text prefix
+        if isOpenAICompatibility && readBuffer.getString(at: 0, length: 5) == "data:" {
+            readBuffer.moveReaderIndex(forwardBy: 5)
+        }
+        guard let bodyData = readBuffer.readDataUntilNewline() ?? readBuffer.readData(length: readBuffer.readableBytes),
+              let json = try? JSONSerialization.jsonObject(with: bodyData, options: [])
+        else {
+            print()
+            Self.logger.error("Invalid JSON Data: \(buffer.getString(at: 0, length: buffer.readableBytes) ?? "")")
+            return // not a valid JSON response or possibly only part of the message
+        }
+
+        Self.logger.debug("json: \(json)")
+
+        guard let jsonObject = json as? [String: Any] else {
+            Self.logger.error("\(url) returned not a JSON object: \(json)")
+            return
+        }
+
+
+        if isOpenAICompatibility {
+            let object = jsonObject["object"] as? String
+            if object == "chat.completion.chunk" {
+                if let message = (jsonObject["choices"] as? [[String: Any]])?[0],
+                   let delta = message["delta"] as? [String: Any]
+                {
+                    if let content = delta["content"] as? String {
+                        print(content, terminator: "")
+                    }
+                    if let content = message["content"] as? String {
+                        print(content, terminator: "")
+                    }
+                }
+            }
+        } else {
+            if let message = jsonObject["message"] as? [String: Any],
+               let done = jsonObject["done"] as? Bool,
+               done == false
+            {
                 if let content = message["thinking"] as? String {
                     print(content, terminator: "")
                 }
@@ -42,6 +96,10 @@ struct OllamaLogger {
                     print(content, terminator: "")
                 }
             }
+        }
+
+        if readBuffer.readableBytes > 1 {
+            print("")
         }
     }
 
